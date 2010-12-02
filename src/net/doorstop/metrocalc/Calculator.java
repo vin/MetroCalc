7135ff25ffa3d278419cadb89f1109091ff25452
Vineet Kumar <vineet@doorstop.net>
2010-12-02 01:41:43 -0500

    Fix rounding error.

--- src/net/doorstop/metrocalc/Calculator.java
+++ src/net/doorstop/metrocalc/Calculator.java
@@ -9,19 +9,14 @@ import android.widget.TextView;
 
 public class Calculator extends Activity {
 
-    private static int TABLE[] = {
-        1565, 1365, 1165, 965, 2135,
-        1935, 1735, 1535, 1335, 1135,
-        935, 930, 2100, 1900, 1700,
-        1500, 1300, 1100, 900, 2265,
-        2065, 1865, 1665, 1465, 1265,
-        1065, 865, 2035, 1835, 1635,
-        1435, 1235, 1035, 835, 830,
-        2000, 1800, 1600, 1400, 1200,
-        1000, 800, 2165, 1965, 1765
-    };
+    private static int TABLE[] = { 1565, 1365, 1165, 965, 2135, 1935, 1735,
+            1535, 1335, 1135, 935, 930, 2100, 1900, 1700, 1500, 1300, 1100,
+            900, 2265, 2065, 1865, 1665, 1465, 1265, 1065, 865, 2035, 1835,
+            1635, 1435, 1235, 1035, 835, 830, 2000, 1800, 1600, 1400, 1200,
+            1000, 800, 2165, 1965, 1765 };
 
     private static final int BONUS_MULTIPLIER = 15;
+    private static final int BONUS_THRESHOLD = 800;
     private int currentCents = 200;
 
     /** Called when the activity is first created. */
@@ -33,7 +28,7 @@ public class Calculator extends Activity {
     }
 
     public void addCents(View v) {
-        addCents(Integer.valueOf((String)v.getTag()));
+        addCents(Integer.valueOf((String) v.getTag()));
     }
 
     public void addCents(int n) {
@@ -42,19 +37,28 @@ public class Calculator extends Activity {
         refresh();
     }
 
+    public int computeBonus(int amount) {
+        if (amount < BONUS_THRESHOLD) {
+            return 0;
+        } else {
+            return (amount * BONUS_MULTIPLIER + 50) / 100;
+        }
+    }
+
     private void refresh() {
         setTextViewText(R.id.currentValue, formatCents(currentCents));
-        int suggestion = computeSuggestionWithBonus(currentCents);
+        int suggestion = lookupSuggestion(currentCents);
         setTextViewText(R.id.suggestion, formatCents(suggestion));
-        int bonus = suggestion * BONUS_MULTIPLIER / 100;
+        int bonus = computeBonus(suggestion);
         setTextViewText(R.id.bonus, formatCents(bonus));
         int newTotal = currentCents + suggestion + bonus;
         setTextViewText(R.id.newTotal, formatCents(newTotal));
     }
 
-    public static int computeSuggestionWithBonus(int cents) {
+    public static int lookupSuggestion(int cents) {
         if (cents % 5 != 0) {
-            throw new IllegalArgumentException("input must be nonnegative multiple of 5.");
+            throw new IllegalArgumentException(
+                    "input must be nonnegative multiple of 5.");
         }
         int i = cents % 225 / 5;
         return TABLE[i];
